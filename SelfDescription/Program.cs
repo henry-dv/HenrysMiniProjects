97f57edb78008f8051543dbde0d7246950c949a7
JHLehmann <henning.lehmann@gicom.org>
2019-10-11 14:09:38 +0200

    hello

---
+++ SelfDescription/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace SelfDescription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello :) it's me again
+
+            //I'm currently writing something into a console by putting a comment into a C# script...
+
+            Console.Write(File.ReadAllText(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName, "Program.cs")));
+            Console.ReadLine();
+        }
+    }
+}
